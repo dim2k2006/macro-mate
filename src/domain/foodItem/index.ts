b09201b96122d7faf280bcb8ce4700cf7ffdd146
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-04-19 13:03:06 +0200

    Add index.ts

---
+++ src/domain/foodItem/index.ts
@@ -0,0 +1,5 @@
+export type { FoodItem } from './foodItem.model.ts';
+export type { FoodItemRepository } from './foodItem.repository.ts';
+export type { FoodItemService } from './foodItem.service.ts';
+export { default as FoodItemServiceImpl } from './foodItem.service.impl.ts';
+export { default as FoodItemRepositoryCorrelate } from './foodItem.repository.local.ts';
