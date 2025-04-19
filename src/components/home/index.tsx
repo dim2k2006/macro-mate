2bc472d55cf5d948766e728bba02ff9b33025c5e
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-04-19 17:43:55 +0200

    Add Layout

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,9 +1,24 @@
+import { Box, TextInput } from '@mantine/core';
+
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
-      <p className="text-lg">This is the home page of your application.</p>
-    </div>
+    <Box p="md">
+      <TextInput
+        label="Описание блюда"
+        placeholder="Введите описание блюда"
+        value=""
+        onChange={(e) => console.log(e.currentTarget.value)}
+        required
+      />
+
+      <TextInput
+        label="Название блюда"
+        placeholder="Введите название блюда"
+        value=""
+        onChange={(e) => console.log(e.currentTarget.value)}
+        required
+      />
+    </Box>
   );
 }
 
