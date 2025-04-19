54afef506fea2a973b143e6a9d2bf3c697b8695d
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-04-19 22:34:25 +0200

    Update home

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,32 @@
-import { AppShell, Group, Image } from '@mantine/core';
+import { AppShell, Group, Image, Tabs } from '@mantine/core';
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 function Layout({ children }: LayoutProps) {
+  const { t } = useTranslation();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [activeTab, setActiveTab] = React.useState<string | null>(location.pathname);
+
+  function handleTabChange(value: string | null) {
+    if (!value) {
+      return;
+    }
+
+    setActiveTab(value);
+
+    navigate(value, { replace: true });
+  }
+
   return (
     <AppShell
       padding={0}
       header={{
+        height: 60,
+      }}
+      footer={{
         height: 60,
       }}
     >
@@ -16,6 +37,15 @@ function Layout({ children }: LayoutProps) {
       </AppShell.Header>
 
       <AppShell.Main>{children}</AppShell.Main>
+
+      <AppShell.Footer p="sm">
+        <Tabs value={activeTab} onChange={handleTabChange}>
+          <Tabs.List justify="center">
+            <Tabs.Tab value="/">{t('home')}</Tabs.Tab>
+            <Tabs.Tab value="/food">{t('foodItems')}</Tabs.Tab>
+          </Tabs.List>
+        </Tabs>
+      </AppShell.Footer>
     </AppShell>
   );
 }
