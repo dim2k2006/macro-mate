96e3b65c82049a2441ed320461a978ead0e25b93
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-04-19 22:07:57 +0200

    Update home

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,34 +1,106 @@
-import { Box, TextInput, Button, Textarea } from '@mantine/core';
-import { useForm, hasLength, isEmail } from '@mantine/form';
+import { Box, TextInput, Button, Textarea, NativeSelect, NumberInput, Space, SimpleGrid } from '@mantine/core';
+import { useForm, hasLength } from '@mantine/form';
 import { useTranslation } from 'react-i18next';
+import { Unit } from '@/domain/foodItem';
+
+const units: Unit[] = ['g', 'ml'];
 
 function Home() {
   const { t } = useTranslation();
 
   const form = useForm({
     mode: 'controlled',
-    initialValues: { name: '', email: '' },
+    initialValues: {
+      description: '',
+      name: '',
+      unit: 'g' as const,
+      calories: undefined,
+      proteins: undefined,
+      fats: undefined,
+      carbs: undefined,
+    },
     validate: {
       name: hasLength({ min: 3 }, 'Must be at least 3 characters'),
-      email: isEmail('Invalid email'),
     },
   });
 
-  function handleSubmit(values: { name: string; email: string }) {
+  function handleSubmit(values: {
+    description: string;
+    name: string;
+    unit: Unit;
+    calories: number | undefined;
+    proteins: number | undefined;
+    fats: number | undefined;
+    carbs: number | undefined;
+  }) {
     console.log('values:', values);
   }
 
   return (
     <Box p="md">
       <form onSubmit={form.onSubmit(handleSubmit)}>
-        <Textarea label={t('foodItemLabel')} placeholder={t('foodItemPlaceholder')} autosize minRows={10} />
+        <Textarea
+          {...form.getInputProps('description')}
+          label={t('foodItemLabel')}
+          placeholder={t('foodItemPlaceholder')}
+          autosize
+          minRows={10}
+        />
 
-        <TextInput {...form.getInputProps('name')} label="Name" placeholder="Name" />
+        <Button type="button" mt="md" color="teal" fullWidth>
+          {t('calculateMacros')}
+        </Button>
 
-        <TextInput {...form.getInputProps('email')} mt="md" label="Email" placeholder="Email" />
+        <Space h="md" />
 
-        <Button type="submit" mt="md">
-          Submit
+        <TextInput
+          {...form.getInputProps('name')}
+          label={t('foodItemLabel')}
+          placeholder={t('foodItemNamePlaceholder')}
+        />
+
+        <Space h="md" />
+
+        <NativeSelect {...form.getInputProps('unit')} label={t('foodItemUnitLabel')} data={units} />
+
+        <Space h="md" />
+
+        <NumberInput
+          {...form.getInputProps('calories')}
+          label={t('foodItemCaloriesLabel')}
+          placeholder={t('foodItemCaloriesPlaceholder')}
+        />
+
+        <Space h="md" />
+
+        <SimpleGrid cols={3}>
+          <div>
+            <NumberInput
+              {...form.getInputProps('proteins')}
+              label={t('foodItemProteinLabel')}
+              placeholder={t('foodItemProteinPlaceholder')}
+            />
+          </div>
+
+          <div>
+            <NumberInput
+              {...form.getInputProps('fats')}
+              label={t('foodItemFatLabel')}
+              placeholder={t('foodItemFatPlaceholder')}
+            />
+          </div>
+
+          <div>
+            <NumberInput
+              {...form.getInputProps('carbs')}
+              label={t('foodItemCarbsLabel')}
+              placeholder={t('foodItemCarbsPlaceholder')}
+            />
+          </div>
+        </SimpleGrid>
+
+        <Button type="submit" mt="md" fullWidth>
+          {t('saveFoodItem')}
         </Button>
       </form>
     </Box>
--- src/domain/foodItem/index.ts
+++ src/domain/foodItem/index.ts
@@ -1,4 +1,4 @@
-export type { FoodItem } from './foodItem.model.ts';
+export type { FoodItem, Unit } from './foodItem.model.ts';
 export type { FoodItemRepository } from './foodItem.repository.ts';
 export type { FoodItemService, CreateFoodItemInput } from './foodItem.service.ts';
 export { default as FoodItemServiceImpl } from './foodItem.service.impl.ts';
