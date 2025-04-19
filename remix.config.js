1ab3f48accc93b6fac0febf4dade9aec7b93fd43
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-04-19 12:32:08 +0200

    Update deps

--- remix.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-  serverBuildTarget: 'node-cjs',
-  server: './server.js',
-  appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
-  publicPath: '/build/',
-};
