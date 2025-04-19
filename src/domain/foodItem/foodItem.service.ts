17190de834a33b2eb8c3870cef553c5d0d013c8b
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-04-19 12:50:50 +0200

    Add service

---
+++ src/domain/foodItem/foodItem.service.ts
@@ -0,0 +1,21 @@
+import { FoodItem } from './foodItem.model.ts';
+
+export interface FoodItemService {
+  createFoodItem: (input: CreateFoodItemInput) => Promise<FoodItem>;
+  getFoodItemById: (id: string) => Promise<FoodItem>;
+  listFoodItems: () => Promise<FoodItem[]>;
+  updateFoodItem: (id: string, foodItem: FoodItem) => Promise<FoodItem>;
+  deleteFoodItem: (id: string) => Promise<void>;
+  calculateMacros: (id: string) => Promise<FoodItem>;
+}
+
+export type CreateFoodItemInput = {
+  state: 'cooking' | 'cooked';
+  unit: 'g' | 'ml';
+  description: string;
+  name: string;
+  calories?: number;
+  proteins?: number;
+  fats?: number;
+  carbs?: number;
+};
