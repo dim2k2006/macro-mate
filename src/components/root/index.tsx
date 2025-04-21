1b68afd054a2bce4ada8dce9d570084f67a99dd4
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-04-21 20:06:06 +0200

    Update root

--- src/components/root/index.tsx
+++ src/components/root/index.tsx
@@ -3,6 +3,7 @@ import { useGetLlmKey } from '@/components/llmKey-service-provider';
 import { buildConfig, buildContainer } from '@/container';
 import ConfigProvider from '@/components/config-provider';
 import { FoodItemServiceProvider } from '@/components/foodItem-service-provider';
+import { MealServiceProvider } from '@/components/meal-service-provider';
 import { Routes, Route } from 'react-router-dom';
 import Intro from '@/components/intro';
 import Home from '@/components/home';
@@ -22,21 +23,23 @@ function Root() {
   return (
     <ConfigProvider config={config}>
       <FoodItemServiceProvider service={container.foodItemService}>
-        <Layout>
-          <Routes>
-            <React.Fragment>
-              <Route path="/" element={<ProtectedRoute />}>
-                <Route path="/" element={<Home />} />
-              </Route>
+        <MealServiceProvider service={container.mealService}>
+          <Layout>
+            <Routes>
+              <React.Fragment>
+                <Route path="/" element={<ProtectedRoute />}>
+                  <Route path="/" element={<Home />} />
+                </Route>
 
-              <Route path="/food" element={<ProtectedRoute />}>
-                <Route path="/food" element={<Food />} />
-              </Route>
+                <Route path="/food" element={<ProtectedRoute />}>
+                  <Route path="/food" element={<Food />} />
+                </Route>
 
-              <Route path="/intro" element={<Intro />} />
-            </React.Fragment>
-          </Routes>
-        </Layout>
+                <Route path="/intro" element={<Intro />} />
+              </React.Fragment>
+            </Routes>
+          </Layout>
+        </MealServiceProvider>
       </FoodItemServiceProvider>
     </ConfigProvider>
   );
