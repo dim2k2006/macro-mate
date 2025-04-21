cb7efc34f3d1a3f4e20123c04fa33daf9a506274
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-04-21 19:58:43 +0200

    Add MealServiceImpl

---
+++ src/domain/meal/index.ts
@@ -0,0 +1,5 @@
+export type { Meal } from './meal.model.ts';
+export type { MealRepository } from './meal.repository.ts';
+export type { MealService, CreateMealInput } from './meal.service.ts';
+export { default as MealServiceImpl } from './meal.service.impl.ts';
+export { default as MealRepositoryLocal } from './meal.repository.local.ts';
