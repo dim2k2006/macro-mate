fe2d9d606a83eb20ec090cba2719b35c76b5a1b3
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-04-21 20:50:26 +0200

    Update styles

--- src/components/meal/index.tsx
+++ src/components/meal/index.tsx
@@ -1,10 +1,63 @@
+import { useState } from 'react';
+import { Box, Text, Stack, Button } from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
+import dayjs from 'dayjs';
+import isLeapYear from 'dayjs/plugin/isLeapYear';
+import dayOfYear from 'dayjs/plugin/dayOfYear';
+
+dayjs.extend(isLeapYear);
+dayjs.extend(dayOfYear);
+
+const days = getDaysOfCurrentYear();
+
 function Meal() {
+  const todayDayOfYear = dayjs().dayOfYear() - 1;
+
+  const [activeIndex, setActiveIndex] = useState(todayDayOfYear);
+  const handleSlideChange = (index: number) => {
+    setActiveIndex(index);
+  };
+
   return (
-    <div>
-      <h1>Meal</h1>
-      <p>This is the meal component.</p>
-    </div>
+    <Box p="md">
+      <Carousel
+        slideSize={30}
+        height={36}
+        slideGap="xs"
+        align="center"
+        loop
+        withControls={false}
+        initialSlide={activeIndex}
+      >
+        {days.map((day, index) => {
+          const isToday = index === todayDayOfYear;
+
+          const isActive = index === activeIndex;
+
+          const variant = isToday ? (isActive ? 'filled' : 'light') : isActive ? 'filled' : 'default';
+
+          return (
+            <Carousel.Slide key={day.toString()}>
+              <Stack justify="center" align="center">
+                <Button variant={variant} autoContrast onClick={() => handleSlideChange(index)}>
+                  <Text size="xs" style={{ width: '100%', textAlign: 'center' }}>
+                    {day.format('MMM D')}, {day.format('dd')}
+                  </Text>
+                </Button>
+              </Stack>
+            </Carousel.Slide>
+          );
+        })}
+      </Carousel>
+    </Box>
   );
+}
+
+function getDaysOfCurrentYear(): dayjs.Dayjs[] {
+  const start = dayjs().startOf('year');
+  const daysInYear = start.isLeapYear() ? 366 : 365;
+
+  return Array.from({ length: daysInYear }, (_, i) => start.add(i, 'day'));
 }
 
 export default Meal;
