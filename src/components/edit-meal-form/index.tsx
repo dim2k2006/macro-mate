77eb0e8c88bba38f66e15d07ccaa620bab523a09
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-04-30 16:21:22 +0200

    Update form

--- src/components/edit-meal-form/index.tsx
+++ src/components/edit-meal-form/index.tsx
@@ -2,9 +2,24 @@ import { EnhancedMeal, Meal } from '@/domain/meal';
 import { hasLength, useForm } from '@mantine/form';
 import { useTranslation } from 'react-i18next';
 import { useUpsertMeal, useDeleteMeal } from '@/components/meal-service-provider';
-import { Button, Card, NumberInput, Space, Select, Text, Popover, Group } from '@mantine/core';
+import {
+  Button,
+  Card,
+  NumberInput,
+  Space,
+  Text,
+  Popover,
+  Group,
+  Grid,
+  ScrollArea,
+  Stack,
+  Divider,
+} from '@mantine/core';
 import { useListFoodItems } from '@/components/foodItem-service-provider';
 import dayjs from 'dayjs';
+import { useFuzzySearch } from '@/components/fuzzy-search';
+import { useMemo } from 'react';
+import FoodItemCard from '@/components/create-meal-form/food-item-card.tsx';
 
 function EditMealForm({ meal, onFinish }: EditMealFormProps) {
   const { t } = useTranslation();
@@ -14,8 +29,9 @@ function EditMealForm({ meal, onFinish }: EditMealFormProps) {
   const foodItems = foodItemsState.data || [];
 
   const foodItemOptions = foodItems.map((item) => ({
-    label: `${item.name} (${dayjs(item.createdAt).format('YYYY-MM-DD')})`,
-    value: item.id,
+    id: item.id,
+    name: item.name,
+    date: dayjs(item.createdAt).format('YYYY-MM-DD'),
   }));
 
   const { mutate: upsertMeal, isPending: isUpserting, isError: isUpsertError } = useUpsertMeal(meal.id);
@@ -27,6 +43,7 @@ function EditMealForm({ meal, onFinish }: EditMealFormProps) {
     initialValues: {
       foodItemId: meal.foodItemId,
       amount: meal.amount,
+      query: '',
     },
     validate: {
       foodItemId: hasLength({ min: 3 }, t('requiredField')),
@@ -62,6 +79,10 @@ function EditMealForm({ meal, onFinish }: EditMealFormProps) {
     });
   }
 
+  function handleSelectFoodItem(id: string) {
+    form.setFieldValue('foodItemId', id);
+  }
+
   function handleDelete() {
     deleteMeal(undefined, {
       onSuccess: () => {
@@ -74,25 +95,78 @@ function EditMealForm({ meal, onFinish }: EditMealFormProps) {
     });
   }
 
+  const { search, highlightText } = useFuzzySearch(foodItemOptions, {
+    includeScore: true,
+    includeMatches: true,
+    threshold: 0.4,
+    keys: ['name', 'date'],
+    findAllMatches: true,
+    minMatchCharLength: 1,
+  });
+
+  const searchResults = useMemo(() => {
+    if (!form.values.query) return [];
+
+    return search(form.values.query).map((result) => ({
+      item: result.item,
+      matches: result.matches,
+    }));
+  }, [form.values.query, search]);
+
+  const results =
+    searchResults.length > 0 ? searchResults : foodItemOptions.map((option) => ({ item: option, matches: [] }));
+
+  const selectedFoodItem = foodItems.find((item) => item.id === form.values.foodItemId);
+
   const isPending = isUpserting || isDeleting;
   const isError = isUpsertError || isDeleteError;
 
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
       <form onSubmit={form.onSubmit(handleSubmit)}>
-        <Select
-          {...form.getInputProps('foodItemId')}
-          label={t('selectFoodItem')}
-          data={foodItemOptions}
-          searchable
-          disabled={isPending}
-        />
+        {!!selectedFoodItem && (
+          <Grid>
+            <Grid.Col span={8}>
+              <Text fw={500}>{selectedFoodItem.name}</Text>
+
+              <Text size="xs">{dayjs(selectedFoodItem.createdAt).format('YYYY-MM-DD')}</Text>
+            </Grid.Col>
+
+            <Grid.Col span={4}>
+              <Button variant="outline" size="compact-xs" onClick={() => handleSelectFoodItem('')} color="red">
+                {t('clear')}
+              </Button>
+            </Grid.Col>
+          </Grid>
+        )}
 
         <Space h="md" />
 
         <NumberInput {...form.getInputProps('amount')} label={t('mealAmount')} disabled={isPending} />
 
         <Space h="md" />
+
+        <ScrollArea h={280}>
+          <Stack>
+            {results.map(({ item, matches }) => {
+              const valueMatches = matches?.filter((match) => match.indices[0][0] >= 0 && match.value === item.name);
+
+              return (
+                <>
+                  <FoodItemCard
+                    key={item.id}
+                    id={item.id}
+                    name={highlightText(item.name, valueMatches)}
+                    date={item.date}
+                    onSelect={handleSelectFoodItem}
+                  />
+
+                  <Divider />
+                </>
+              );
+            })}
+          </Stack>
+        </ScrollArea>
 
         <Group justify="center">
           <Button type="submit" mt="md" color="teal" fullWidth>
