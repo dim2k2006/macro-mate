b6f79028bf77c0e97b1454c02b5e9c0aedcf7aab
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-05-06 20:08:47 +0200

    Add SettingsServiceImpl

---
+++ src/domain/settings/index.ts
@@ -0,0 +1,5 @@
+export type { Settings } from './settings.model.ts';
+export type { SettingsRepository } from './settings.repository.ts';
+export type { SettingsService } from './settings.service.ts';
+export { default as SettingsServiceImpl } from './settings.service.impl.ts';
+export { default as SettingsRepositoryLocal } from './settings.repository.local.ts';
