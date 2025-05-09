a72fe5760fb15f0593a3621ba50a753afaf0547b
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-05-09 21:14:56 +0200

    Update food

--- src/components/food/index.tsx
+++ src/components/food/index.tsx
@@ -1,10 +1,14 @@
 import { useListFoodItems } from '@/components/foodItem-service-provider';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import CookingFoodItem from '@/components/cooking-foodItem';
-import { Box, Space } from '@mantine/core';
+import { Box, Space, TextInput } from '@mantine/core';
 import React from 'react';
+import { useFuzzySearch } from '@/components/fuzzy-search';
+import { useTranslation } from 'react-i18next';
 
 function Food() {
+  const { t } = useTranslation();
+
   const foodItemsState = useListFoodItems();
 
   const foodItems = useMemo(() => {
@@ -19,15 +23,44 @@ function Food() {
     return [];
   }, [foodItemsState.data, foodItemsState.isError, foodItemsState.isLoading, foodItemsState.isSuccess]);
 
+  const [query, setQuery] = useState('');
+
+  const { search } = useFuzzySearch(foodItems, {
+    includeScore: true,
+    includeMatches: true,
+    threshold: 0.4,
+    keys: ['name', 'date'],
+    findAllMatches: true,
+    minMatchCharLength: 1,
+  });
+
+  const searchResults = useMemo(() => {
+    if (!query) return [];
+
+    return search(query).map((result) => ({
+      item: result.item,
+      matches: result.matches,
+    }));
+  }, [query, search]);
+
+  const results =
+    searchResults.length > 0 ? searchResults : foodItems.map((foodItem) => ({ item: foodItem, matches: [] }));
+
   return (
     <Box p="md">
-      {foodItems.map((foodItem) => (
-        <React.Fragment key={foodItem.id}>
-          <CookingFoodItem foodItem={foodItem} isExpanded={false} />
+      <TextInput onInput={(event) => setQuery(event.currentTarget.value)} label={t('selectFoodItem')} />
 
-          <Space h="md" />
-        </React.Fragment>
-      ))}
+      <Space h="md" />
+
+      {results.map(({ item }) => {
+        return (
+          <React.Fragment key={item.id}>
+            <CookingFoodItem foodItem={item} isExpanded={false} />
+
+            <Space h="md" />
+          </React.Fragment>
+        );
+      })}
     </Box>
   );
 }
