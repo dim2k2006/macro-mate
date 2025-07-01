90e7cdc5a4643cffc51d7d762ac423e722ced285
Dmitry Malofeev <dim2k2006@yandex.ru>
2025-07-01 21:51:56 +0200

    Update MacroRecognition

--- src/components/macro-recognition/index.tsx
+++ src/components/macro-recognition/index.tsx
@@ -1,19 +1,56 @@
-import React from 'react';
+import { useState } from 'react';
 import { useRecognizeMacrosFromImage } from '@/components/foodItem-service-provider';
+import { IconCameraAi, IconTrash } from '@tabler/icons-react';
+import { useDisclosure } from '@mantine/hooks';
+import { ActionIcon, Modal, Button, SimpleGrid, Text, Image, Space } from '@mantine/core';
+import { useTranslation } from 'react-i18next';
+import { Dropzone, IMAGE_MIME_TYPE, FileWithPath } from '@mantine/dropzone';
 
 function MacroRecognition() {
-  const { mutate } = useRecognizeMacrosFromImage();
+  const { t } = useTranslation();
 
-  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
+  const { mutate, isPending } = useRecognizeMacrosFromImage();
 
-    console.log('file:', file);
+  const [opened, { open, close }] = useDisclosure(false);
 
-    if (!file) return;
+  const [files, setFiles] = useState<FileWithPath[]>([]);
 
-    console.log('Recognizing macros from file:', file.name);
+  function handleDrop(acceptedFiles: FileWithPath[]) {
+    setFiles((prev) => [...prev, ...acceptedFiles]);
+  }
 
-    mutate(file, {
+  const previews = files.map((file, index) => {
+    const imageUrl = URL.createObjectURL(file);
+
+    const handleRemove = () => {
+      setFiles((prev) => prev.filter((_, i) => i !== index));
+      URL.revokeObjectURL(imageUrl);
+    };
+
+    // return <Image key={index} src={imageUrl} onLoad={() => URL.revokeObjectURL(imageUrl)} />;
+    return (
+      <div key={index} style={{ position: 'relative' }}>
+        <Image src={imageUrl} radius="md" />
+        <ActionIcon
+          variant="light"
+          color="red"
+          size="sm"
+          onClick={handleRemove}
+          style={{
+            position: 'absolute',
+            top: 5,
+            right: 5,
+            backgroundColor: 'white',
+          }}
+        >
+          <IconTrash size={16} />
+        </ActionIcon>
+      </div>
+    );
+  });
+
+  const handleFileChange = async () => {
+    mutate(files[0], {
       onSuccess: (foodItem) => {
         console.log('Macros recognized:', foodItem);
       },
@@ -23,7 +60,29 @@ function MacroRecognition() {
     });
   };
 
-  return <input type="file" accept="image/*" onChange={handleFileChange} />;
+  return (
+    <>
+      <ActionIcon variant="default" size="lg" onClick={open}>
+        <IconCameraAi size={20} />
+      </ActionIcon>
+
+      <Modal opened={opened} onClose={close} title={t('aiProductRecognition')} size="lg" centered>
+        <Dropzone accept={IMAGE_MIME_TYPE} onDrop={handleDrop}>
+          <Text ta="center">{t('attachProductImages')}</Text>
+        </Dropzone>
+
+        <Space h="md" />
+
+        <SimpleGrid cols={3}>{previews}</SimpleGrid>
+
+        <Space h="md" />
+
+        <Button variant="outline" onClick={handleFileChange} disabled={files.length === 0 || isPending}>
+          {t('recognizeProduct')}
+        </Button>
+      </Modal>
+    </>
+  );
 }
 
 export default MacroRecognition;
